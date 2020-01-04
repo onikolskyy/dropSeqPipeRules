039bfb6aa5321d0a5fbaa02fd0ef725efa2c89e8
o <o.nikolskyy@meanware.de>
2020-01-04 11:22:34 +0100

    2.1.0

--- scripts/tag_with_true_barcodes.py
+++ scripts/tag_with_true_barcodes.py
@@ -1,27 +1,16 @@
 from Bio import SeqIO
-from sortedcontainers import SortedList
 from collections import defaultdict
 import pysam
 import gzip
 import sys
 
-
 discard_secondary_alignements = snakemake.params['discard_secondary_alignements']
 fastq_parser = SeqIO.parse(gzip.open(snakemake.input[0], "rt"), "fastq")
-read_barcodes = defaultdict(lambda :{'XC':'','XM':''})
 infile_bam = pysam.AlignmentFile(snakemake.input[1], "rb")
 outfile = pysam.AlignmentFile(snakemake.output[0], "wb", template=infile_bam)
 
-bins = []
-
-def find_in_bins(bins, ref):
-    for bin in bins:
-        try:
-            i = bin[1].index(ref)
-            return i
-        except:
-            continue
-    return -1
+mapping=defaultdict(dict)
+read_barcodes = defaultdict(lambda :{'XC':'','XM':''})
 
 barcodes_struct = {
 	'BC_start':snakemake.params['BC_start'],
@@ -30,42 +19,39 @@ barcodes_struct = {
 	'UMI_end':snakemake.params['UMI_end']
 	}
 
-
-print("parsing witelist _________ \n")
+print("parsing witelist")
 
 #construct bins
 with open(snakemake.input[2],'r') as whitelist:
     for line in whitelist:
         print("parse line \n")
-        if len(line.strip().split()) == 2:  # This means we didn't find any other linked barcode
-            (reference, counts_ref) = line.strip().split()
-            bins.append ([reference, SortedList()]) #Push an empty bin and tag it with reference barcode
-        else:
+        if len(line.strip().split()) > 2:  # This means we didn't find any other linked barcode
             (reference, extended_ref, counts_ref, counts_ext) = line.strip().split()
-            list = SortedList()
             for barcode in extended_ref.split(','):
-                list.add(barcode)
-            bins.append([reference, list])
+                mapping[barcode]["ref"]=reference
+            mapping[reference]["ref"]=reference
+        else:
+            (reference, counts_ref) = line.strip().split()
+            mapping[reference]["ref"] = reference
 
-print("Bins ready. Total amount", len(bins), "\n")
+print("parsing whitelist done")
 
 #write umi/bc to dict
 for fastq_R1 in fastq_parser:
-    ref = str(fastq_R1.seq)[barcodes_struct['BC_start']:barcodes_struct['BC_end']]      #barcode
+    bc = str(fastq_R1.seq)[barcodes_struct['BC_start']:barcodes_struct['BC_end']]      #barcode
     umi = str(fastq_R1.seq)[barcodes_struct['UMI_start']:barcodes_struct['UMI_end']]    #umi
-    if(ref==''):
+    if(bc==''):
         sys.SystemExit('UMI empty for read {}.\n The barcode is: {}.\nWhole entry is:{}'.format(fastq_R1.id, fastq_R1.seq,fastq_R1))
-
     #repair bc
-    index = find_in_bins(bins, ref)
-    print("bin index", index, "\n")
-    true_ref = bins[index][0] if index >= 0 else ref
+    map=mapping[bc]
+    found="ref" in map
+    true_bc=map["ref"] if found else bc
     #write
-
-    read_barcodes[fastq_R1.id]['XC'] = true_ref
+    read_barcodes[fastq_R1.id]['XC'] = true_bc
     read_barcodes[fastq_R1.id]['XM'] = umi
+    print("saving tags for ID", fastq_R1.id, " -- ", ("found" if found else "did not found"), " reference in mapping")
 
-print("read_barcodes dict constructed \n start tagging bam \n")
+print("start tagging bam")
 
 #tag each read in bam with umi/bc from dict
 for bam_read in infile_bam:
