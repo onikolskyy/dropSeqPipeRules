35ea352d5dda58abf009c609d62f486a068fc75b
o <o.nikolskyy@meanware.de>
2020-02-23 12:30:41 +0100

    fix exons

--- TEST/TagReadWithGeneExon/test/scripts/bin/helperClasses/geneIntervalTree/refflat_entries.py
+++ TEST/TagReadWithGeneExon/test/scripts/bin/helperClasses/geneIntervalTree/refflat_entries.py
@@ -38,11 +38,11 @@ class RefflatEntries:
         },
         "exon_starts" : {
             "index": 9,
-            "convert_func": lambda exon_starts: exon_starts.split(",")
+            "convert_func": lambda exon_starts: [int(exon_start) for exon_start in exon_starts.split(",")]
         },
         "exon_ends" : {
             "index": 10,
-            "convert_func": lambda exon_ends: exon_ends.split(",")
+            "convert_func": lambda exon_ends: [int(exon_end) for exon_end in exon_ends.split(",")]
         },
     }
 
