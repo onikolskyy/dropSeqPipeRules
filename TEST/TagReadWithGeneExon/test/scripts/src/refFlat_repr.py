66a3d00dbbe6b2750d4948cab5b535752e423b7a
o <o.nikolskyy@meanware.de>
2020-04-16 11:56:16 +0200

    correct coordinates

--- TEST/TagReadWithGeneExon/test/scripts/src/refFlat_repr.py
+++ TEST/TagReadWithGeneExon/test/scripts/src/refFlat_repr.py
@@ -29,7 +29,7 @@ class RefflatEntries:
         },
         "coding_start": {
             "index": 6,
-            "convert_func": lambda start: int(start)+1
+            "convert_func": lambda start: int(start)
         },
         "coding_end": {
             "index": 7,
@@ -41,7 +41,7 @@ class RefflatEntries:
         },
         "exon_starts" : {
             "index": 9,
-            "convert_func": lambda exon_starts: [int(exon_start)+1 for exon_start in exon_starts.split(",")[:-1]] if (len(exon_starts) > 0 and not re.compile(" +\B").match(exon_starts)) else []
+            "convert_func": lambda exon_starts: [int(exon_start) for exon_start in exon_starts.split(",")[:-1]] if (len(exon_starts) > 0 and not re.compile(" +\B").match(exon_starts)) else []
         },
         "exon_ends" : {
             "index": 10,
