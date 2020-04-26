bcaa2983906ef18eead56d7d625262393d282430
o <o.nikolskyy@meanware.de>
2020-04-26 11:53:52 +0200

    fix init script

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ rm -R /dropSeqPipe/rules &&
 	cp -R rules /dropSeqPipe && 
 	cp whitelist_opts.csv / &&
 	cp  envs/* /dropSeqPipe/envs && 
-	cp scripts/* /dropSeqPipe/scripts
+	cp -R scripts/* /dropSeqPipe/scripts
