122d7b3b809d5b2ead0d09c37e9250d098f62eda
o <o.nikolskyy@meanware.de>
2020-04-30 09:05:59 +0200

    fix output

--- TEST/TagReadWithGeneExon/test/scripts/test.py
+++ TEST/TagReadWithGeneExon/test/scripts/test.py
@@ -1,30 +1,35 @@
+import itertools
 import pysam
 import math
 import numpy as np
 import pandas as pd
 from ncls import NCLS
 from time import time
-from multiprocessing import Pool, Array
+from multiprocessing import Pool, RawArray
 
 #from bin.helperClasses.geneIntervalTree.gene_interval_tree import GeneIntervalTree
 from src.refFlat_repr import RefFlatParsed
 
-
 GLOB_REFFLAT = {}
 
-def initializer(sharr, shape):
-    GLOB_REFFLAT["sharr"] = sharr
-    GLOB_REFFLAT["shape"] = shape
+
+def initializer(glob_intervals, glob_intervals_shape):
+    GLOB_REFFLAT["intervals"] = glob_intervals
+    GLOB_REFFLAT["intervals_shape"] = glob_intervals_shape
+
 
 def worker(blocks_list):
     LFs = pd.DataFrame({"name": ["INTERGENIC", "INTRONIC", "UTR", "CODING"]})
 
-    sharr_np = np.frombuffer(GLOB_REFFLAT["sharr"]).reshape(GLOB_REFFLAT["shape"])
-    refFlat_intervals = pd.DataFrame(sharr_np,collumns=["gene","start","end","LF"])
-
+    # get intervals from shared memory
+    sharr_np = np.frombuffer(GLOB_REFFLAT["intervals"],dtype=np.int64).reshape(GLOB_REFFLAT["intervals_shape"])
+    reference = pd.DataFrame(sharr_np,columns=["gene","start","end","LF"])
     # init NCLS
-    ncl = NCLS(refFlat_intervals.start.to_numpy(), refFlat_intervals.end.to_numpy(), refFlat_intervals.index.to_numpy())
-
+    ncl = NCLS(
+        np.ascontiguousarray(reference.start.to_numpy()),
+        np.ascontiguousarray(reference.end.to_numpy()),
+        np.ascontiguousarray(reference.index.to_numpy())
+    )
     # unpack blocks and create pandas
     blocks_list_unpacked = [blocks_list[b][i] for b in range(len(blocks_list)) for i in range(len(blocks_list[b]))]
     ranges_start = [np.arange(blocks_list_unpacked[b][0], blocks_list_unpacked[b][1]) for b in
@@ -54,7 +59,7 @@ def worker(blocks_list):
     overlaps = pd.DataFrame({"I1": query_index, "I2": ncl_index})
     merged = (overlaps \
               .merge(reads, left_on="I1", right_index=True) \
-              .merge(refFlat_intervals[["gene", "LF"]], left_on="I2", right_index=True)) \
+              .merge(reference[["gene", "LF"]], left_on="I2", right_index=True)) \
         [["read", "block", "start", "gene", "LF"]]
 
     # how many distinct B's does an R have?
@@ -82,44 +87,67 @@ def worker(blocks_list):
     multi_block = multi_block.merge(LFs, left_on="LF", right_index=True)[["read", "gene", "name"]]
 
     # concat splitted data
-    res = pd.concat([multi_block, single_block]).merge(LFs, right_index=True, left_on="LF")[["read", "gene", "LF"]]
+    res = pd.concat([multi_block, single_block])
 
     return res
 
 ####################################################################################
 
-
-
 #bam file
 infile_bam = pysam.AlignmentFile(snakemake.input["inbam"], "rb")
-outfile = pysam.AlignmentFile(snakemake.output[0], "wb", template=infile_bam)
 #parse refflat
 refFlat = RefFlatParsed(snakemake.input["refflat"], infile_bam)
-
+outfile = pysam.AlignmentFile(snakemake.output[0], "wb", template=infile_bam)
 
 #################################################################################
 # make reads data frame
 #################################################################################
 
 
-reads = [read for read in infile_bam]
-refs = [infile_bam.getrname(reads[r].tid) for r in range(len(reads))]
-refs_df = pd.DataFrame({"ref":refs})
+def chunked_iterable(iterable, size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, size))
+        if not chunk:
+            break
+        yield chunk
 
-for ref, group in refs_df.groupby("ref"):
-    reads_for_ref[ref] = [reads[i] for i in group.index.to_list()]
 
-packed_blocks_for_ref = {}
-for ref in refs:
-    packed_blocks_for_ref[ref] = [reads_for_ref[ref][r].get_blocks() for r in range(len(reads_for_ref[ref]))]
+bam_header = infile_bam.header
+reads_chunked = chunked_iterable(infile_bam,5*10**6)
 
-for ref, packed_blocks in packed_blocks_for_ref.items():
-    refFlat_intervals =  refFlat.as_intervals(ref).to_numpy()
-    sharr = Array('c_int64', refFlat_intervals.shape[0]*refFlat_intervals.shape(1), False)
-    sharr_np = np.frombuffer(sharr).reshape(refFlat_intervals.shape)
-    np.copyto(sharr_np,refFlat_intervals)
+chunk_ctr = 1
+tot_reads = 0
+for chunk in reads_chunked:
+    reads_from_chunk = [r for r in chunk]
+    print("chunk", chunk_ctr, len(reads_from_chunk))
+    blocks_packed = [reads_from_chunk[i].get_blocks() for i in range(len(reads_from_chunk))]
+    refs = pd.DataFrame({"ref": [infile_bam.getrname(reads_from_chunk[r].tid) for r in range(len(reads_from_chunk))]})
+    for ref, group in refs.groupby("ref"):
 
-    with Pool(processes=snakemake.threads, initializer=initializer, initargs=(sharr,refFlat_intervals.shape)) as pool:
-        results = pool.map(worker, packed_blocks_for_ref[ref], chunksize=10**6)
+        # get reference data
+        reference_data = refFlat.as_intervals(ref)
+        if reference_data is not None:
+            gene, refFlat_intervals = reference_data[0], reference_data[1]
+            # store packed blocks
+            chunk_for_ref = [blocks_packed[i] for i in group.index.to_list()]
+            iterator_over_chunk = chunked_iterable(chunk_for_ref, 500000)
+            intervals = refFlat_intervals[["gene","start","end","LF"]].to_numpy()
+
+            # store it in shared memory
+            sharr = RawArray('l', intervals.shape[0]*intervals.shape[1])
+            sharr_np = np.frombuffer(sharr, dtype=np.int64).reshape(intervals.shape)
+            np.copyto(sharr_np, intervals)
+
+            with Pool(processes=snakemake.threads, initializer=initializer,
+                      initargs=(sharr, intervals.shape)) as pool:
+                results = pool.map(worker, iterator_over_chunk)
+    print("finished chunk",chunk_ctr)
+
+    #TODO: write data to output bam!
+
+    chunk_ctr+= 1
+    tot_reads+= len(reads_from_chunk)
+print("TOTAL:", len(reads_from_chunk))
 
 
